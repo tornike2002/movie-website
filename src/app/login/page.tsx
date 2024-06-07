1c32dd97b0f05df62b95d7374c47d6262f4cff23
tornike2002 <toko.toka2000@gmail.com>
2024-06-07 18:09:29 +0400

    added basic form for login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,29 @@
+"use client";
+import Image from "next/image";
+import styled from "styled-components";
+import LoginIcon from "/public/loginAssets/MovieIcon.png";
 export default function LoginPage() {
-  return <div>IM login page</div>;
+  return (
+    <LoginContainer>
+      <Image src={LoginIcon} alt="hello" />
+      <div>
+        <h1></h1>
+        <div>
+          <input type="text" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <div>
+          <button>Login to your account</button>
+          <div>
+            <button>Do not have account?</button>
+            <button>Sign up</button>
+          </div>
+        </div>
+      </div>
+    </LoginContainer>
+  );
 }
+
+const LoginContainer = styled.section`
+  min-height: 100vh;
+`;
