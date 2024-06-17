b1bc51b788bdbdcc989279932fb1c324b788b5f9
tornike2002 <toko.toka2000@gmail.com>
2024-06-17 20:07:50 +0400

    finished design of login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -34,11 +34,17 @@ export default function LoginPage() {
             className="login-inputs-style"
           />
         </div>
-        <div>
-          <button type="submit">Login to your account</button>
-          <div>
-            <button>Do not have account?</button>
-            <button>Sign up</button>
+        <div className="login-buttons">
+          <button type="submit" className="login-account">
+            Login to your account
+          </button>
+          <div className="login-end-buttons">
+            <button type="button" className="end-buttons-question">
+              Do not have account?
+            </button>
+            <button type="button" className="end-buttons-sign">
+              Sign up
+            </button>
           </div>
         </div>
       </LoginForm>
@@ -47,20 +53,42 @@ export default function LoginPage() {
 }
 
 const LoginContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   min-height: 100vh;
+  gap: 82.99px;
 `;
 
 const LoginForm = styled.form`
   display: flex;
   flex-direction: column;
   background-color: #161d2f;
+  font-family: "Outfit", sans-serif;
+  padding: 32px;
+  border-radius: 16px;
   h1 {
     color: #ffff;
-    font-family: "Outfit", sans-serif;
     font-size: 32px;
     font-weight: 400;
     line-height: 40.32px;
     margin-bottom: 40px;
+  }
+  .login-buttons {
+    .login-account {
+      background-color: rgba(252, 71, 71, 1);
+      font-size: 15px;
+      font-weight: 400;
+      line-height: 18.9px;
+      text-align: center;
+      color: rgba(255, 255, 255, 1);
+      padding: 14px 96.5px;
+      outline: none;
+      border: none;
+      border-radius: 8px;
+      margin-top: 40px;
+    }
   }
   .login-inputs {
     display: flex;
@@ -73,6 +101,32 @@ const LoginForm = styled.form`
       border-bottom: 1px solid #5a698f;
       color: #ffffff;
       min-height: 37px;
+    }
+  }
+  .login-end-buttons {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    margin-top: 24px;
+
+    .end-buttons-question {
+      color: rgba(255, 255, 255, 1);
+      font-size: 15px;
+      line-height: 18.9px;
+      background: none;
+      border: none;
+      outline: none;
+      cursor: pointer;
+    }
+
+    .end-buttons-sign {
+      color: rgba(252, 71, 71, 1);
+      font-size: 15px;
+      line-height: 18.9px;
+      background: none;
+      border: none;
+      outline: none;
+      cursor: pointer;
     }
   }
 `;
