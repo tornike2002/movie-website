7576ef309ec6af1b81fd4a612f17ec389936fca5
tornike2002 <toko.toka2000@gmail.com>
2024-06-18 17:59:52 +0400

    react icons installed

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
 import NavLogo from "/public/loginAssets/MovieIcon.png";
+import { AppstoreOutlined ,PlaySquareOutlined} from "@ant-design/icons";
+
 export default function Navbar() {
   return (
     <div>
-      <Image src={NavLogo} alt="icon" />
+      <div>
+        <Image src={NavLogo} alt="icon" />
+        <AppstoreOutlined style={{color: "white"}}/>
+        <PlaySquareOutlined style={{color: "white"}}/>
+  
+      </div>
     </div>
   );
 }
