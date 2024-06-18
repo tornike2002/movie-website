b084b291b42fa96502ad0f35415cef1ee20fb496
tornike2002 <toko.toka2000@gmail.com>
2024-06-18 18:05:09 +0400

    added four icon for navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
 import NavLogo from "/public/loginAssets/MovieIcon.png";
-import { AppstoreOutlined ,PlaySquareOutlined} from "@ant-design/icons";
 
+import { AiFillAppstore } from "react-icons/ai";
+import { MdLocalMovies } from "react-icons/md";
+import { FaBookmark } from "react-icons/fa";
+import { PiTelevisionDuotone } from "react-icons/pi";
 export default function Navbar() {
   return (
     <div>
       <div>
         <Image src={NavLogo} alt="icon" />
-        <AppstoreOutlined style={{color: "white"}}/>
-        <PlaySquareOutlined style={{color: "white"}}/>
-  
+        <AiFillAppstore style={{ color: "white" }} />
+        <MdLocalMovies style={{ color: "white" }} />
+        <PiTelevisionDuotone style={{ color: "white" }} />
+        <FaBookmark style={{ color: "white" }} />
       </div>
     </div>
   );
